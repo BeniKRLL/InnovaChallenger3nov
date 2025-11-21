3a9d2af700a828835e5238e4bd6a15bfd99f3d7b
BeniKRLL <joaobamatias@gmail.com>
2025-11-21 17:25:26 -0300

    Finalização

--- Beni/script.js
+++ Beni/script.js
@@ -1,14 +1,8 @@
-function irPara(pagina) {
-  window.location.href = pagina;
-}
+const sidebar = document.getElementById("sidebar");
+const toggleBtn = document.getElementById("toggle-btn");
+const container = document.querySelector(".container");
 
-function toggleMenu() {
-  const sidebar = document.getElementById("sidebar");
-  const container = document.querySelector(".container");
+toggleBtn.addEventListener("click", () => {
   sidebar.classList.toggle("closed");
   container.classList.toggle("full");
-}
-
-function abrirLink() {
-  window.open("https://www.youtube.com", "_blank");
-}
+});
